19b390672442c88eb51a956bcf1b7c13a2b75187
Thiago Henrique <thiagohdeplima@gmail.com>
2025-03-27 19:05:28 -0300

    feat: creates first app version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nixos/nix:latest
+FROM nixos/nix:latest AS build
 
 WORKDIR /usr/local/src
 
@@ -6,4 +6,16 @@ COPY . .
 
 RUN nix-build
 
+RUN mkdir -p /srv/app
+RUN cp -pvr `nix-store -qR result/` /srv/app
 
+FROM busybox
+
+ENV PYTHONPATH "/srv/app/.virtualenv/lib/python3.12/site-packages/"
+
+WORKDIR /srv/app
+
+COPY --from=build /srv/app /nix/store
+COPY --from=build /usr/local/src/result /srv/app
+
+CMD ["/srv/app/.virtualenv/bin/gunicorn", "-b 0.0.0.0", "main:app"]
--- default.nix
+++ default.nix
@@ -1,11 +1,31 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-
 let
-  pkgs = nixpkgs.pkgs;
-
-  to_install = [
-    pkgs.python312
-    pkgs.python312Packages.pip
-  ];
+  pkgs = import <nixpkgs> {};
 in
-  to_install
+
+pkgs.stdenv.mkDerivation {
+  name = "application-poetry";
+
+  src     = /usr/local/src/app;
+  version = "0.1.0";
+
+  propagatedBuildInputs = [
+    pkgs.bash
+    pkgs.poetry
+    pkgs.python312
+  ];
+
+  buildCommand = ''
+    export HOME=$out;
+    export VIRTUAL_ENV=$out/.virtualenv;
+
+    python -m venv $VIRTUAL_ENV;
+    cp -r $src/* $out;
+
+    echo "${pkgs.python312}/bin/python main.py" > $out/startup
+    chmod +x $out/startup;
+
+    cd $out;
+    poetry 
+    poetry install;
+  '';
+}
